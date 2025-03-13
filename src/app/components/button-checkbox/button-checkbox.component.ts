b9996f3ca2aa2034775da4ccacb5f14f7ba366f5
Naquos <maxence.delfanti@yahoo.fr>
2025-03-13 23:15:39 +0100

    Ajout de tooltips sur les boutons + suppressions d'images inutiles

--- src/app/components/button-checkbox/button-checkbox.component.ts
+++ src/app/components/button-checkbox/button-checkbox.component.ts
@@ -1,15 +1,17 @@
 import { Component, input } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-button-checkbox',
-  imports: [],
+  imports: [MatTooltipModule],
   templateUrl: './button-checkbox.component.html',
   styleUrl: './button-checkbox.component.scss'
 })
 export class ButtonCheckboxComponent {
   public srcImg = input<String>("");
   public control = input<FormControl<boolean>>();
+  public tooltip = input<string>("");
   protected enabled = false;
 
   protected changeValue() {
