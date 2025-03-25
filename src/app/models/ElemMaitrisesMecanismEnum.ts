153e5c2f0660b84b26a9aa24a038d92081b164f9
Naquos <maxence.delfanti@yahoo.fr>
2025-03-25 16:28:56 +0100

    Changement wording

--- src/app/models/ElemMaitrisesMecanismEnum.ts
+++ src/app/models/ElemMaitrisesMecanismEnum.ts
@@ -1,9 +1,9 @@
 export enum ElemMaitrisesMecanismEnum {
-    ABNEGATION = "Abnégation",
-    ALTERNANCE = "Alternance",
-    ALTERNANCE_2 = "Alternance 2",
-    ANATOMIE = "Anatomie",
-    CONCENTRATION_ELEMENTAIRE = "Concentration élémentaire",
-    INFLEXIBILITE_2 = "Infléxibilité 2",
+    ABNEGATION = "Abnégation (Pré-nerf)",
+    ALTERNANCE = "Alternance (Pré-nerf)",
+    ALTERNANCE_2 = "Alternance 2 (Pré-nerf)",
+    ANATOMIE = "Anatomie (Pré-nerf)",
+    CONCENTRATION_ELEMENTAIRE = "Concentration élémentaire (Pré-nerf)",
+    INFLEXIBILITE_2 = "Infléxibilité 2 (Pré-nerf)",
     COEUR_HUPPERMAGE = "Coeur huppermage"
 }
