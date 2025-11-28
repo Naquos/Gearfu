bbb0215b10e9e160cc27078c0d576c61680c813e
Naquos <maxence.delfanti@yahoo.fr>
2025-11-28 13:26:57 +0100

    Ajout de nouvelles images

--- src/app/directives/imageFallback.directive.ts
+++ src/app/directives/imageFallback.directive.ts
@@ -26,6 +26,21 @@ export class ImageFallbackDirective {
 
   private imageMap = new Map<GfxIdMobEnum, string>([
     [GfxIdMobEnum.YDALIPE, "mob/ydalipe.png"],
+    [GfxIdMobEnum.BILBYGIRL, "mob/bilbygirl.png"],
+    [GfxIdMobEnum.BILBYBOY, "mob/bilbyboy.png"],
+    [GfxIdMobEnum.GELEE_REGLISSE, "mob/gelee-reglisse.png"],
+    [GfxIdMobEnum.ASSASSIN_ENCAPUCHONNE, "mob/assassin-encapuchonne.png"],
+    [GfxIdMobEnum.PROTECTEUR_ENCAPUCHONNE, "mob/protecteur-encapuchonne.png"],
+    [GfxIdMobEnum.RATCHITIK, "mob/ratchitik.png"],
+    [GfxIdMobEnum.LELA, "mob/lela.png"],
+    [GfxIdMobEnum.ERPEL, "mob/erpel.png"],
+    [GfxIdMobEnum.EENCA, "mob/eenca.png"],
+    [GfxIdMobEnum.RATAGASD_LE_MARRON, "mob/ratagasd-le-marron.png"],
+    [GfxIdMobEnum.MOUMOUTE_LA_POSTICHE, "mob/moumoute-la-postiche.png"],
+    [GfxIdMobEnum.SCARABOSS, "mob/scaraboss.png"],
+    [GfxIdMobEnum.SCARAFON, "mob/scarafon.png"],
+    [GfxIdMobEnum.SCARAMEL, "mob/scaramel.png"],
+    [GfxIdMobEnum.SCARABRUNI, "mob/scarabruni.png"],
   ]);
 
 
