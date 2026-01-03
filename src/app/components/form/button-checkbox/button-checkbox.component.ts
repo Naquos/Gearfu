f6c26ab2dcc25c79c53f8911756906fc2a9dfd55
Naquos <maxence.delfanti@yahoo.fr>
2026-01-03 23:27:13 +0100

    Fix bug hydratation

--- src/app/components/form/button-checkbox/button-checkbox.component.ts
+++ src/app/components/form/button-checkbox/button-checkbox.component.ts
@@ -1,13 +1,13 @@
 
 import { Component, input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-button-checkbox',
-  imports: [MatTooltipModule, MatIconModule, TranslateModule, MatIconModule],
+  imports: [MatTooltipModule, MatIconModule, TranslateModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './button-checkbox.component.html',
   styleUrl: './button-checkbox.component.scss',
 })
@@ -17,4 +17,10 @@ export class ButtonCheckboxComponent {
   public readonly control = input<FormControl<boolean>>();
   public readonly tooltip = input<string>("");
   public readonly crossedOut = input<boolean>(false);
+
+  protected toggle(): void {
+    this.control()?.setValue(!this.control()?.value);
+    this.control()?.markAsDirty();
+    this.control()?.markAsTouched();
+  }
 }
