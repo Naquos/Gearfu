7c4c910db74254f05205819139e229087d58aca9
Naquos <maxence.delfanti@yahoo.fr>
2026-03-18 22:06:57 +0100

    Ajout de skeletons dans la recherche de builds

--- src/app/components/search-pages/search-build-liste/search-build-liste.component.ts
+++ src/app/components/search-pages/search-build-liste/search-build-liste.component.ts
@@ -1,38 +1,48 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SupabaseService } from '../../../services/supabase/supabaseService';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { BuildComponent } from "../../items-pages/build/build.component";
 import { FilterSearchBuildFormService } from '../../../services/form/filterSearchBuildFormService';
-import { debounceTime, switchMap } from 'rxjs';
+import { debounceTime, finalize, switchMap } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-search-build-liste',
-  imports: [BuildComponent],
+  imports: [BuildComponent, TranslateModule],
   templateUrl: './search-build-liste.component.html',
   styleUrl: './search-build-liste.component.scss',
 })
 export class SearchBuildListeComponent {
   private readonly supabaseService = inject(SupabaseService);
   private readonly filterSearchBuildFormService = inject(FilterSearchBuildFormService);
+  protected readonly isLoading = signal(true);
+  protected readonly skeletonArray = Array.from({ length: 8 }, (_, i) => i);
+  protected readonly skeletonSlotsArray = Array.from({ length: 14 }, (_, i) => i);
 
   protected builds = toSignal(
     this.filterSearchBuildFormService.result$.pipe(
       debounceTime(300),
-      switchMap(result => this.supabaseService.getBuildsListByFilter(
-        result.levelMin,
-        result.levelMax,
-        result.class,
-        result.orderBy,
-        result.PA,
-        result.PM,
-        result.PW,
-        result.PO,
-        result.CC,
-        result.parade,
-        result.sublimationEpique,
-        result.sublimationRelique,
-        result.idItems,
-        result.sublimations,
-      ))), { initialValue: [] });
+      switchMap(result => {
+        this.isLoading.set(true);
+        return this.supabaseService.getBuildsListByFilter(
+          result.levelMin,
+          result.levelMax,
+          result.class,
+          result.orderBy,
+          result.PA,
+          result.PM,
+          result.PW,
+          result.PO,
+          result.CC,
+          result.parade,
+          result.sublimationEpique,
+          result.sublimationRelique,
+          result.idItems,
+          result.sublimations,
+        ).pipe(
+          finalize(() => this.isLoading.set(false)),
+        );
+      }),
+    ), { initialValue: [] });
 
 }
