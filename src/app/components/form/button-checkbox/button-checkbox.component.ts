dfb396a7688e964656344acd83d132127e093361
Naquos <maxence.delfanti@yahoo.fr>
2026-06-20 23:05:04 +0200

    Correction sur le sélecteur d'éléments

--- src/app/components/form/button-checkbox/button-checkbox.component.ts
+++ src/app/components/form/button-checkbox/button-checkbox.component.ts
@@ -1,10 +1,13 @@
 
 import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { FormField, FieldTree } from '@angular/forms/signals';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
+import { NEVER } from 'rxjs';
+import { startWith, switchMap } from 'rxjs/operators';
 import { LazyImageDirective } from '../../../directives/lazy-image.directive';
 
 @Component({
@@ -24,11 +27,21 @@ export class ButtonCheckboxComponent {
   public readonly tooltip = input<string>("");
   public readonly crossedOut = input<boolean>(false);
   protected readonly inputId = `button-checkbox-${ButtonCheckboxComponent.nextId++}`;
+
+  private readonly _controlValue = toSignal(
+    toObservable(this.control).pipe(
+      switchMap(control =>
+        control ? control.valueChanges.pipe(startWith(control.value)) : NEVER
+      )
+    ),
+    { initialValue: false }
+  );
+
   protected readonly isChecked = computed(() => {
     const field = this.fieldControl();
     if (field) {
       return field().value();
     }
-    return this.control()?.value ?? false;
+    return this._controlValue() ?? false;
   });
 }
