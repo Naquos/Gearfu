7fc42ef74a6782225d6c4bd73abe57791f748db7
Naquos <maxence.delfanti@yahoo.fr>
2026-06-21 06:28:49 +0200

    ajout de tooltips sur les mécaniques

--- src/app/components/form/modifier-mecanism/modifier-mecanism.component.ts
+++ src/app/components/form/modifier-mecanism/modifier-mecanism.component.ts
@@ -4,11 +4,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { TranslateModule } from '@ngx-translate/core';
 import { ModifierMecanismFormService } from '../../../services/form-signal/modifierElemMaitrisesFormService';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @Component({
   selector: 'app-modifier-mecanism',
-  imports: [MatFormFieldModule, MatSelectModule, TranslateModule],
+  imports: [MatFormFieldModule, MatSelectModule, TranslateModule, MatTooltipModule],
   templateUrl: './modifier-mecanism.component.html',
   styleUrl: './modifier-mecanism.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -16,4 +17,10 @@ import { ModifierMecanismFormService } from '../../../services/form-signal/modif
 export class ModifierMecanismComponent {
   protected readonly modifierMecanismFormService = inject(ModifierMecanismFormService);
   protected readonly ElemMaitrisesMecanismEnumList = Object.values(Mecanism);
+  protected readonly mapTranslation: Record<Mecanism, string> = {
+    [Mecanism.COEUR_HUPPERMAGE]: 'modifier-mecanism.coeur-huppermage',
+    [Mecanism.DENOUEMENT]: 'modifier-mecanism.denouement',
+    [Mecanism.DEMESURE]: 'modifier-mecanism.demesure',
+    [Mecanism.CHAOS]: 'modifier-mecanism.chaos'
+  };
 }
